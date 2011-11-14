fba039a0ca58b47d66fdfa2655116cdd09ea6865
Jonas Obrist <jonas.obrist@divio.ch>
2011-11-14 15:35:10 +0100

    fixed tons of issues

--- cmsplugin_disqus/cms_plugins.py
+++ cmsplugin_disqus/cms_plugins.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from cmsplugin_disqus import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,10 +10,11 @@ class DisqusPlugin(CMSPluginBase):
     render_template = 'cmsplugin_disqus/disqus_plugin.html'
     admin_preview = False
     name = _('Disqus Plugin')
+    model = models.DisqusPlugin
     
     def render(self, context, instance, placeholder):
         context['DISQUS_SHORTNAME'] = settings.DISQUS_SHORTNAME
         context['instance'] = instance
-        context['developer_mdoe'] = settings.DEBUG
+        context['developer_mode'] = settings.DEBUG
         return context
 plugin_pool.register_plugin(DisqusPlugin)
